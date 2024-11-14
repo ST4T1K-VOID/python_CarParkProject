668ef8d39d2a0ccac75bcc283a94600ba9b300b9
ST4T1K <mjobrien06@hotmail.com>
2024-11-14 13:13:57 +0800

    implement CarPark class constructor and attributes

--- carkParkProject/src/carpark.py
+++ carkParkProject/src/carpark.py
@@ -1,2 +1,8 @@
 class CarPark:
-    pass
+    def __init__(self, location='unknown', capacity=20, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors
+        self.displays = displays
+
