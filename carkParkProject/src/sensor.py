57c831a70cc66dc6132182ddec30903927213ca3
ST4T1K <mjobrien06@hotmail.com>
2024-11-18 17:11:30 +0800

    add detect_vehicle abstrat method

--- carkParkProject/src/sensor.py
+++ carkParkProject/src/sensor.py
@@ -1,8 +1,17 @@
-class Sensor:
+from abc import ABC, abstractmethod
+import random
+
+class Sensor(ABC):
     def __init__(self, id, is_active, carpark):
         self.id = id
         self.is_active = is_active
         self.carpark = carpark
+
+    @abstractmethod
+    def detect_vehicle(self, plate):
+        pass
+
+
 
     def __str__(self):
         return f'Sensor {self.id} | Status >>> {self.is_active}'
